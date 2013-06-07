8cc498ad63af4ef7e253f42af82d1c3a2d74e8df
Nicholas Tung <ntung@yelp.com>
2013-06-07 15:43:47 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="vimap",
+    version="0.1.0-SNAPSHOT",
+    provides=["vimap"],
+    author="gatoatigrado",
+    author_email="gatoatigrado@gmail.com",
+    url="https://github.com/gatoatigrado/vimap",
+    description='vimap',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+    ],
+    install_requires=['testify'],
+    packages=["vimap"],
+    long_description="""vimap -- variations on imap, not in C
+
+The vimap package is designed to provide a more flexible alternative for multiprocessing.imap_unordered.
+It aspires to support HTTP-like clients processing data, though contains nothing client-specific.
+"""
+)
