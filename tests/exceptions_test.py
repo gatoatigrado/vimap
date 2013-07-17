54ecf15cbace5ffe55cc21c5f7ea57153adfa584
David Selassie <dselassi@yelp.com>
2013-07-17 10:05:10 -0700

    Test mixed output and exceptions being yielded from workers.

--- tests/exceptions_test.py
+++ tests/exceptions_test.py
@@ -33,38 +33,43 @@ def serialize_error(error):
     return (type(error), str(error))
 
 
+def run_test_pool(worker_f):
+    '''Start a 3 worker pool of worker_f and pass [1, 10) into
+    it.'''
+    num_workers = 3
+    processes = vimap.pool.fork(
+        worker_f.init_args(init=i)
+        for i
+        in range(num_workers)
+    )
+    # Give every worker something to chew on.
+    res = list(processes.imap(list(range(1, 10))).zip_in_out_typ())
+    # They should all be done by this point.
+    T.assert_equal(processes.finished_workers, True)
+    return res
+
+
 class ExceptionsTest(T.TestCase):
     @mock.patch.object(vimap.exception_handling, 'print_exception', autospec=True)
     def test_exception_before_iteration(self, print_exc_mock):
-        num_workers = 3
-        processes = vimap.pool.fork(worker_raise_exc_immediately.init_args(init=i)
-            for i in range(num_workers))
-        # Give every worker something to chew on.
-        res = list(processes.imap(list(range(1, 10))).zip_in_out_typ())
         res_to_compare = [
             (inp, serialize_error(ec.value), typ)
             for inp, ec, typ
-            in res
+            in run_test_pool(worker_raise_exc_immediately)
         ]
         # Each worker will stop processing once an exception makes it to
         # the top so we only get that number of exceptions back out.
         expected_res_to_compare = [
             (vimap.pool.NO_INPUT, serialize_error(ValueError("hello")), 'exception'),
-        ] * num_workers
+        ] * 3
         T.assert_sorted_equal(res_to_compare, expected_res_to_compare)
-        T.assert_equal(processes.finished_workers, True)
 
     @mock.patch.object(vimap.exception_handling, 'print_exception', autospec=True)
     def test_exception_after_iteration_not_returned(self, print_exc_mock):
-        num_workers = 3
-        processes = vimap.pool.fork(worker_raise_exc_after_iteration.init_args(init=i)
-            for i in range(num_workers))
-        # Give every worker something to chew on.
-        res = list(processes.imap(list(range(1, 10))).zip_in_out_typ())
         res_to_compare = [
             (inp, out, typ)
             for inp, out, typ
-            in res
+            in run_test_pool(worker_raise_exc_after_iteration)
         ]
         # The pool notices that all output has been returned, so doesn't
         # wait for any more responses. We shouldn't see exceptions.
@@ -74,33 +79,29 @@ class ExceptionsTest(T.TestCase):
             in range(1, 10)
         ]
         T.assert_sorted_equal(res_to_compare, expected_res_to_compare)
-        T.assert_equal(processes.finished_workers, True)
 
     @mock.patch.object(vimap.exception_handling, 'print_exception', autospec=True)
     def test_exception_with_curleys(self, print_exc_mock):
         '''Dumb test ... I aim to write tests for most every bug that had existed,
         but this is kinda 1-off ... (.format() got a curley brace).
         '''
-        num_workers = 3
-        processes = vimap.pool.fork(worker_raise_exc_with_curleys.init_args(init='{a}')
-            for _ in range(num_workers))
-        # Give every working something to chew on.
-        res = list(processes.imap(list(range(1, 10))).zip_in_out_typ())
         res_to_compare = [
             (serialize_error(ec.value), typ)
             for _, ec, typ
-            in res
+            in run_test_pool(worker_raise_exc_with_curleys)
         ]
         # Each worker will stop processing once an exception makes it to
         # the top so we only get that number of exceptions back out.
         expected_res_to_compare = [
+            # We're not sure which inputs will get picked, but all
+            # should return this exception.
             (serialize_error(ValueError("{0} curley braces!")), 'exception'),
-        ] * num_workers
+        ] * 3
         T.assert_sorted_equal(res_to_compare, expected_res_to_compare)
-        T.assert_equal(processes.finished_workers, True)
 
     @mock.patch.object(vimap.exception_handling, 'print_exception', autospec=True)
     def test_unconsumed_exceptions(self, print_exc_mock):
+        '''Unconsumed exceptions should only be printed.'''
         processes = vimap.pool.fork(worker_raise_exc_immediately.init_args(init=i)
             for i in [1, 1, 1])
         del processes
@@ -126,8 +127,28 @@ class ExceptionsTest(T.TestCase):
         processes = vimap.pool.fork((worker_raise_exc_with_curleys.init_args(init=i)
             for i in xrange(100)), in_queue_size_factor=2)
         processes.imap([-1] * 3000 + list(range(50)))
-        del processes
 
+        # Check yielded output.
+        res_to_compare = []
+        for inp, out, typ in processes.zip_in_out_typ():
+            if typ == 'exception':
+                res_to_compare.append((inp, serialize_error(out.value), typ))
+            else:
+                res_to_compare.append((inp, out, typ))
+        # All the -1s will produce None output.
+        expected_res_to_compare = [
+            (-1, None, 'output')
+        ] * 3000
+        # Once we get to the positive numbers, we start causing 50 of
+        # the 100 workers to throw exceptions.
+        expected_res_to_compare.extend([
+            (i, serialize_error(ValueError("{0} curley braces!")), 'exception')
+            for i
+            in range(50)
+        ])
+        T.assert_sorted_equal(res_to_compare, expected_res_to_compare)
+
+        # Check out exception logging.
         calls = print_exc_mock.call_args_list
         errors = [serialize_error(call_args[0].value) for call_args, _ in calls]
         T.assert_equal(errors, [serialize_error(ValueError("{0} curley braces!"))] * 50)
