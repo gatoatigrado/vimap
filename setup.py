e0b49b744f1c40d8b66e37699f9f1221e2fac9fe
nick tung <gatoatigrado@gmail.com>
2013-12-11 17:14:56 -0800

    cleanup fds version bump (v0.1.4)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="vimap",
-    version="0.1.3",
+    version="0.1.4",
     provides=["vimap"],
     author="gatoatigrado",
     author_email="gatoatigrado@gmail.com",
