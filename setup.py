96a89186e2fd85c1863a164bedaf0e9886eb24de
gatoatigrado <gatoatigrado@gmail.com>
2014-10-14 16:46:30 -0700

    md --> pandoc conversion

--- setup.py
+++ setup.py
@@ -1,5 +1,36 @@
+import os.path
+
 from setuptools import find_packages
 from setuptools import setup
+
+
+# Robust-but-boilerplate code to detect the README path
+base_path = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(base_path, 'README.md')
+assert os.path.isfile(readme_path), "Couldn't find README.md"
+
+
+# Mumbo jumbo to convert Markdown --> RST for PyPI
+#
+# brew install pandoc  # or whatever for your OS
+# pip install pyandoc
+try:
+    import pandoc
+except ImportError:
+    print("WARNING: COULD NOT IMPORT pandoc; YOU WON'T HAVE A NICE README")
+    readme_rst = """vimap -- variations on imap, not in C
+
+The vimap package is designed to provide a more flexible alternative for
+multiprocessing.imap_unordered.  It aspires to support HTTP-like clients
+processing data, though contains nothing client-specific.
+"""
+else:
+    def get_rst(markdown_file):
+        doc = pandoc.Document()
+        doc.markdown = open(markdown_file).read()
+        return doc.rst
+
+    readme_rst = get_rst(readme_path)
 
 
 setup(
@@ -20,10 +51,7 @@ setup(
     ],
     install_requires=[],
     packages=find_packages(exclude=['tests*']),
-    long_description="""vimap -- variations on imap, not in C
 
-The vimap package is designed to provide a more flexible alternative for
-multiprocessing.imap_unordered.  It aspires to support HTTP-like clients
-processing data, though contains nothing client-specific.
-"""
+    # mumbo-jumbo for PyPI :(
+    long_description=readme_rst
 )
